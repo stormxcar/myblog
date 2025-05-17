2c472a386acea3522a277cfea3af4be67b5fd789
nguyen kha <nkha3561@gmail.com>
2025-05-17 13:12:38 +0700

    Update Procfile to serve project/index.php as homepage

--- project/components/connect.php
+++ project/components/connect.php
@@ -1,17 +1,18 @@
 <?php
-
-$servername = 'db'; // Tên service MySQL trong docker-compose.yml
-$username = 'root';
-$password = 'root';
-$dbname = 'blog_db';
+require 'vendor/autoload.php'; // Nếu dùng Composer
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 
 try {
-  // dbname : blog_db
-  $conn = new PDO("mysql:host=$servername;port=3306;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // echo "connect successfully";
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $servername = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $dbname = substr($url["path"], 1);
+
+    $conn = new PDO("mysql:host=$servername;port=3306;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
