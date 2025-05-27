1b3d4f27d481102d3cbbc49025feaf3a56b618ed
nguyen kha <nkha3561@gmail.com>
2025-05-27 14:21:27 +0700

    fix open

--- index.php
+++ index.php
@@ -1,13 +1,4 @@
 <?php
-
-// Kiểm tra nếu URL không tồn tại
-if (!file_exists($_SERVER['REQUEST_URI']) && !is_dir($_SERVER['REQUEST_URI'])) {
-    http_response_code(404);
-    include '404.php';
-    exit;
-}
-
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -17,5 +8,6 @@ if (!file_exists(__DIR__ . '/static/home.php')) {
     die("Error: home.php not found in static directory.");
 }
 
+// Chuyển hướng đến /static/home.php : 
 header("Location: /static/home.php");
 exit;
